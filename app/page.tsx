035beb351abcd09323404ec5865872a8538953ee
Ashu Gupta <ashugpt1812@gmail.com>
2023-12-10 05:23:09 +0530

    change ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,23 @@
 "use client"
 
-import { useState } from 'react';
+import React, { use, useEffect } from 'react';
+import Image from 'next/image';
 import { useAccount } from 'wagmi';
 import axios from 'axios';
-import { useRouter } from 'next/navigation';
 
-export default function Start() {
+export default function Home() {
 
   const { address, isConnected, isConnecting, isDisconnected } = useAccount();
 
-  const router = useRouter();
-  const [email, setEmail] = useState<string>('');
+  const [email, setEmail] = React.useState<string>('');
 
-  // functions
+  const [emailSubmitted, setEmailSubmitted] = React.useState<boolean>(false);
+  const [otpSubmitted, setOtpSubmitted] = React.useState<boolean>(false);
+  const[otp, setOtp] = React.useState<number | null>(null);
 
   async function sendEmailAndAddress() {
     try {
-      const response = await axios.post('http://localhost:3000/mail',
-      {
-        email: email,
-        userAddress: address,
-      }
-      );
+      const response = await axios.post('http://localhost:4000/addmail', { email: email, userAddress: address });
       console.log(`Sent data\nemail: ${email}, address: ${address}`);
       console.log('Response from server:', response.data);
     } catch (error) {
@@ -29,35 +25,74 @@ export default function Start() {
     }
   }
 
-  const handleSubmit = async (e : any) => {
+  function handleSubmitEmail(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
-    await sendEmailAndAddress();
-    console.log(email)
-    router.push('/otp');
-    
-  };
+    sendEmailAndAddress();
+    setEmailSubmitted(true);
+    console.log('Email submitted.');
+  }
+
+  function handleSubmitOtp(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    setOtpSubmitted(true);
+    console.log('OTP submitted.');
+  }
+
+  function handleSignTxn(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    console.log('Signed txn.');
+  }
 
 
-  if (isConnecting) return <main className="flex flex-col items-center justify-between p-24 border border-gray-500 rounded-xl m-4"><div>Connecting…</div></main>
+
+
+
   
-  if (isDisconnected) return <main className="flex flex-col items-center justify-between p-24 border border-gray-500 rounded-xl m-4"><div>Connect wallet to proceed</div></main>
+  return (
+    <div className='flex justify-center items-center h-[80vh]'>
+    <main className="flex flex-col items-center justify-between p-24 font-mono w-full max-w-5xl">
 
-  return (isConnected &&
-    <main className="flex flex-col items-center justify-between p-24 border border-gray-500 rounded-xl m-4">
-        <form className='flex flex-col justify-center items-center space-y-4' onSubmit={handleSubmit}>
-            <div className='flex justify-center items-center space-x-4 p-4'>
-                <label htmlFor="email">Enter Email:</label>
-                <input
-                    className='p-1 rounded-lg text-black' 
-                    type="email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    placeholder="Enter your email"
-                    required
-                />
-            </div>
-        <button className='p-2 w-32 bg-blue-500 font-bold rounded-xl' type="submit">Submit</button>
+      { !isConnected && <div className='text-2xl m-4'>Connect Wallet</div>}
+
+      {isConnected && 
+      
+        <form className='flex flex-col justify-center items-center space-y-4 w-full max-w-5xl' onSubmit={(e) => handleSubmitEmail(e)}>
+          <div className='flex flex-col items-center w-full space-x-4 p-4'>
+            <div className='text-2xl m-4'>Enter Email</div>
+            <input 
+              className='px-2 py-3 text-black w-[60%]' 
+              type="text"
+              name="email" 
+              id="email" 
+              placeholder='abc@xyz'
+              onChange={(e) => {setOtp(e.target.value as any)}}
+            />
+          </div>
+          <button className='p-2 m-4 w-32 bg-blue-500 font-bold' type='submit'>Submit</button>
         </form>
+      }
+
+
+      { isConnected && emailSubmitted && 
+      <div className='flex flex-col justify-center items-center m-4'>
+        <div className='text-green-400 text-lg'>email submitted</div>
+        <form className='flex flex-col justify-center items-center space-y-4' onSubmit={(e) => handleSignTxn(e)}>
+          <div className='flex justify-center items-center space-x-4 p-4'>
+            <div className='text-2xl m-4'>Enter OTP</div>
+            <input 
+              className='px-2 py-3 text-black' 
+              type="text" 
+              name="otp" 
+              id="otp" 
+              placeholder='696969' 
+              onChange={(e) => {setOtp(e.target.value as any)}}
+            />
+          </div>
+          <button className='p-2 w-32 bg-blue-500 font-bold' type='submit'>sign txn</button>
+        </form>
+      </div> }
+
     </main>
-  );
+    </div>
+  )
 }
